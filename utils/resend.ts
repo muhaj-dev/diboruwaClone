e3ecea4c0a0de89211c0e5f16b80beea0a18c3e1
Zeeson <olabrazanislam@gmail.com>
2023-12-02 12:20:25 +0100

    update

--- utils/resend.ts
+++ utils/resend.ts
@@ -1,8 +1,7 @@
 import React from "react";
 import { Resend } from "resend";
 
-// export const resend = new Resend(process.env.RESEND_SECRET);
-export const resend = new Resend("re_Ee9D3su5_4uYDSSe5moEw3E9952YXV9Wq");
+export const resend = new Resend(process.env.RESEND_SECRET as string);
 
 // Define a reusable email sending function
 const sendEmail = async (
