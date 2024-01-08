817af8dccb0043223ae7e764c5631768a51bddc1
Z3phyron-Snides <z3phyronsnides@gmail.com>
2024-01-08 06:28:36 -0800

    app(core): location

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import LocationModal from "@/component/LocationModal";
 import FeedbackCarousel from "@/component/feedBacksCarousel/FeedBacks";
 import Home from "@/containers/Home/Home";
 
 const page = () => {
   return (
-    <Home/>
-  
+    <>
+      <Home />
+      <LocationModal />
+    </>
   );
-}
+};
 
 export default page;
