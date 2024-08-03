9b9ac66e7fdd8c3690fbb500883d2ceda0192fe1
Oyinloye Joseph <oyinjoe23@gmail.com>
2024-08-03 04:04:36 +0100

    feat: Grooming component

--- containers/partnerWithUs/component/barbingservices/BarbingService.tsx
+++ containers/partnerWithUs/component/barbingservices/BarbingService.tsx
@@ -3,37 +3,43 @@ const BarbingService = () => {
   return (
     <section className="barbingservice_container">
       <div className="barbingservice_width">
-        <div className="barbingservice_img">
-          <img
-            src="/images/Rectangle 319.png"
-            className="dbarbingservice_img-content"
-            alt=""
-          />
-        </div>
-        <div className="barbingservice_text-content">
-          <ul className="barbingservice_text-ul">
-            <li className="barbingservice_text-li">Food vendors</li>
-          </ul>
-          <div className="barbingservice_img-text">
+        <div className="barbingservice-img-text">
+          <div className="barbingservice_img">
             <img
-              src="/images/Frame 2611540.png"
-              className="barbingservice_img-text_content"
+              src="/images/Rectangle 319.png"
+              className="barbingservice_img-content"
               alt=""
             />
           </div>
-          <div className="barbingservice_img-transparent">
-            <img
-              src="/images/Ellipse 108.png"
-              className="barbingservice_img-transparent_content"
-              alt=""
-            />
+          <div className="barbingservice_text-content">
+            <ul className="barbingservice_text-ul">
+              <li className="barbingservice_text-li">Grooming</li>
+            </ul>
+            <div className="barbingservice_img-text">
+              <img
+                src="/images/Frame 2611540.png"
+                className="barbingservice_img-text_content"
+                alt=""
+              />
+            </div>
+            <div className="barbingservice_img-transparent">
+              <img
+                src="/images/Ellipse 108.png"
+                className="barbingservice_img-transparent_content"
+                alt=""
+              />
+            </div>
+            <a href="/" className="barbingservice_signup">
+              Sign Up
+            </a>
           </div>
-          <a href="/" className="barbingservice_signup">
-            Sign Up
-          </a>
         </div>
         <div className="barbingservice_overlay-img">
-          <img src="/images/Frame 2609849.png" className="barbingservice_overlay-img_content" alt="" />
+          <img
+            src="/images/Frame 2609849.png"
+            className="barbingservice_overlay-img_content"
+            alt=""
+          />
         </div>
       </div>
     </section>
