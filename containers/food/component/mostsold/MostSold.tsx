f67f6217124bb94fbb3bce629f25c349f3ea4626
Oyinloye Joseph <oyinjoe23@gmail.com>
2024-08-13 11:14:06 +0100

    feat: discount sale component

--- containers/food/component/mostsold/MostSold.tsx
+++ containers/food/component/mostsold/MostSold.tsx
@@ -1,169 +1,74 @@
 import React from "react";
 import "./mostsold.css";
-import Link from "next/link";
-import { IoIosHeartEmpty, IoIosHeart, IoMdStopwatch } from "react-icons/io";
-import { FaBagShopping } from "react-icons/fa6";
-import { MdFavoriteBorder, MdOutlineTimer } from "react-icons/md";
-import { RiArrowRightSLine } from "react-icons/ri";
-import { FaStar } from "react-icons/fa";
+import { DiscountSale, FoodVendor } from "@/containers/Home/components/Data";
 
-const food = [
-  {
-    link: "/",
-    img: "/images/dish.png",
-  },
-  {
-    link: "/",
-    img: "/images/dish.png",
-  },
-  {
-    link: "/",
-    img: "/images/dish.png",
-  },
-  {
-    link: "/",
-    img: "/images/dish.png",
-  },
-  {
-    link: "/",
-    img: "/images/dish.png",
-  },
-];
-const MostSold = () => {
+const MostSold: React.FC = () => {
   return (
     <section className="mostsold_container">
       <div className="mostsold-frame">
-        <p className="mostsold_title">Most Sold</p>
+        <p className="mostsold_title">{FoodVendor[0].title}</p>
         <div className="mostsold-cards">
-          <div className="mostsold-card">
-            <div className="mostsold-card_food-img">
-              <img
-                src="/images/Rectangle 248 (10).png"
-                alt=""
-                className="mostsold-card_img"
-              />
-              <div className="mostsold-card_img-iconcontainer">
-                <MdFavoriteBorder className="mostsold-card_img-icon" />
+          {FoodVendor[0].items.map((item, index) => {
+            // Destructure the Icon components from the item
+            const FavoriteIcon = item.favoriteIcon;
+            const StarIcon = item.starIcon;
+            const TimeIcon = item.timeIcon;
+            const PrizeIcon = item.prizeIcon;
+            return (
+              <div key={index} className="mostsold-card">
+                <div className="mostsold-card_food-img">
+                  <img
+                    src={item.img}
+                    alt={item.smallTitle}
+                    className="mostsold-card_img"
+                  />
+                  <div className="mostsold-card_img-iconcontainer">
+                    <FavoriteIcon className="mostsold-card_img-icon" />
+                  </div>
+                </div>
+                <div className="mostsold-card_content">
+                  <div className="mostsold-card_context">
+                    <div className="mostsold-card_context-top">
+                      <small className="mostsold-card_title">
+                        {item.smallTitle}
+                      </small>
+                      <span className="mostsold-card_dot"></span>
+                      <StarIcon className="mostsold-card_star" />
+                      <small className="mostsold-card_rating">
+                        {item.rating}
+                      </small>
+                    </div>
+                    <div className="mostsold-card_timer">
+                      <TimeIcon className="mostsold-card_timer-icon" />
+                      <span className="mostsold-card_timer-text">
+                        {item.timeText}
+                      </span>
+                    </div>
+                  </div>
+                  <small className="mostsold-card_remender">
+                    {item.remenderText}
+                  </small>
+                  <div className="mostsold-card_prize">
+                    <p className="mostsold-card_prize-text">{item.prizeText}</p>
+                    <a
+                      href={item.prizeLink}
+                      className="mostsold-card_prize-link"
+                    >
+                      <PrizeIcon className="mostsold-card_prize-icon" />
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="mostsold-card_content">
-            <div className="mostsold-card_context">
-              <div className="mostsold-card_context-top">
-                <small className="mostsold-card_title">Fried Rice</small>
-                <span className="mostsold-card_dot"></span>
-                <FaStar className="mostsold-card_star" />
-                <small className="mostsold-card_rating">4.5</small>
-              </div>
-              <div className="mostsold-card_timer">
-                <IoMdStopwatch className="mostsold-card_timer-icon" />
-                <span className="mostsold-card_timer-text">30mins</span>
-              </div>
-            </div>
-            <small className="mostsold-card_remender">
-              10 Liters remaining
-            </small>
-            <div className="mostsold-card_prize">
-              <p className="mostsold-card_prize-text">$40</p>
-              <a href="/cart" className="mostsold-card_prize-link">
-                <FaBagShopping className="mostsold-card_prize-icon" />
-              </a>
-            </div>
-          </div>
-              </div>
-          <div className="mostsold-card">
-            <div className="mostsold-card_food-img">
-              <img
-                src="/images/Rectangle 248 (10).png"
-                alt=""
-                className="mostsold-card_img"
-              />
-              <MdFavoriteBorder className="mostsold-card_img-icon" />
-            </div>
-            <div className="mostsold-card_context">
-              <div className="mostsold-card_context">
-                <small className="mostsold-card_title">Fried Rice</small>
-                <span className="mostsold-card_dot"></span>
-                <FaStar className="mostsold-card_star" />
-                <small className="mostsold-card_rating">4.5</small>
-              </div>
-              <div className="mostsold-card_timer">
-                <IoMdStopwatch className="mostsold-card_timer-icon" />
-                <span className="mostsold-card_timer-text">30mins</span>
-              </div>
-            </div>
-            <small className="mostsold-card_remender">
-              10 Liters remaining
-            </small>
-            <div className="mostsold-card_prize">
-              <p className="mostsold-card_prize-text">$40</p>
-              <a href="/cart" className="mostsold-card_prize-link">
-                <FaBagShopping className="mostsold-card_prize-icon" />
-              </a>
-            </div>
-          </div>
-          <div className="mostsold-card">
-            <div className="mostsold-card_food-img">
-              <img
-                src="/images/Rectangle 248 (10).png"
-                alt=""
-                className="mostsold-card_img"
-              />
-              <MdFavoriteBorder className="mostsold-card_img-icon" />
-            </div>
-            <div className="mostsold-card_context">
-              <div className="mostsold-card_context">
-                <small className="mostsold-card_title">Fried Rice</small>
-                <span className="mostsold-card_dot"></span>
-                <FaStar className="mostsold-card_star" />
-                <small className="mostsold-card_rating">4.5</small>
-              </div>
-              <div className="mostsold-card_timer">
-                <IoMdStopwatch className="mostsold-card_timer-icon" />
-                <span className="mostsold-card_timer-text">30mins</span>
-              </div>
-            </div>
-            <small className="mostsold-card_remender">
-              10 Liters remaining
-            </small>
-            <div className="mostsold-card_prize">
-              <p className="mostsold-card_prize-text">$40</p>
-              <a href="/cart" className="mostsold-card_prize-link">
-                <FaBagShopping className="mostsold-card_prize-icon" />
-              </a>
-            </div>
-          </div>
-          <div className="mostsold-card">
-            <div className="mostsold-card_food-img">
-              <img
-                src="/images/Rectangle 248 (10).png"
-                alt=""
-                className="mostsold-card_img"
-              />
-              <MdFavoriteBorder className="mostsold-card_img-icon" />
-            </div>
-            <div className="mostsold-card_context">
-              <div className="mostsold-card_context">
-                <small className="mostsold-card_title">Fried Rice</small>
-                <span className="mostsold-card_dot"></span>
-                <FaStar className="mostsold-card_star" />
-                <small className="mostsold-card_rating">4.5</small>
-              </div>
-              <div className="mostsold-card_timer">
-                <IoMdStopwatch className="mostsold-card_timer-icon" />
-                <span className="mostsold-card_timer-text">30mins</span>
-              </div>
-            </div>
-            <small className="mostsold-card_remender">
-              10 Liters remaining
-            </small>
-            <div className="mostsold-card_prize">
-              <p className="mostsold-card_prize-text">$40</p>
-              <a href="/cart" className="mostsold-card_prize-link">
-                <FaBagShopping className="mostsold-card_prize-icon" />
-              </a>
-            </div>
-          </div>
+            );
+          })}
         </div>
+ <div className="sale-imgs-container">
+        {DiscountSale.map((item, index) => (
+          <div className="sale-imgs" key={index}>
+            <img src={item.img} alt={item.alt} className="mostsold-sale_img" />
+          </div>
+        ))}
+      </div>
       </div>
     </section>
   );
