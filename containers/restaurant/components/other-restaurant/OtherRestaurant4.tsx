ae9487b71b09adda2cdf45e7307db896dd2630e0
Oyinloye Joseph <oyinjoe23@gmail.com>
2024-09-08 07:39:40 +0100

    feat: restaurant page > other restaurant responsiveness

--- containers/restaurant/components/other-restaurant/OtherRestaurant4.tsx
+++ containers/restaurant/components/other-restaurant/OtherRestaurant4.tsx
@@ -7,43 +7,56 @@ export const OtherRestaurant4: React.FC = () => {
 
   return (
     <div className="other_restaurant_container">
-      <div className="cards">
+      <div className="other_restaurant_cards">
         {OtherFoodResturant.map((item) => (
-          <div key={item._id} className="card">
-            <img className="top-restaurants" src={item.image} alt={item.tag} />
-            <div className="card-details">
-              <div className="text-details">
-                <div className="title_rating_container">
-                  <p className="small-title">{item.smallTitle}</p>
-                  <div className="rating-container">
+          <div key={item._id} className="other_restaurant_card">
+            <img
+              className="other_restaurant_img"
+              src={item.image}
+              alt={item.tag}
+            />
+            <div className="other_restaurant_card-details">
+              <div className="other_restaurant_text-details">
+                <div className="other_restaurant_title_rating_container">
+                  <p className="other_restaurant_small-title">
+                    {item.smallTitle}
+                  </p>
+                  <div className="other_restaurant_rating-container">
                     {item.starIcon &&
                       React.createElement(item.starIcon, {
-                        className: "rating_star_icon",
+                        className: "other_restaurant_rating_star_icon",
                       })}
-                    <small className="rating_num">{item.rating}</small>
-                    <small className="reviews">
+                    <small className="other_restaurant_rating_num">
+                      {item.rating}
+                    </small>
+                    <small className="other_restaurant_reviews">
                       {item.reviewsText} {item.reviewsNum}
                     </small>
                   </div>
                 </div>
-                <div className="location_time_container">
-                  <div className="location">
+                <div className="other_restaurant_location_time_container">
+                  <div className="other_restaurant_location">
                     {item.locationIcon &&
                       React.createElement(item.locationIcon, {
-                        className: "top_rated_loction_icon",
+                        className: "other_restaurant_top_rated_loction_icon",
                       })}
-                    <span className="location-text">{item.locationText}</span>
+                    <span className="other_restaurant_location-text">
+                      {item.locationText}
+                    </span>
                   </div>
-                  <span className="time_dot"></span>
-                  <span className="time">{item.timeNum}</span>
+                  <span className="other_restaurant_time_dot"></span>
+                  <span className="other_restaurant_time">{item.timeNum}</span>
                 </div>
               </div>
 
-              <Link href={`/restaurant/${item._id}`} className="visit-link">
-                <p className="visit-link_text">visit</p>
+              <Link
+                href={`/restaurant/${item._id}`}
+                className="other_restaurant_visit-link"
+              >
+                <p className="other_restaurant_visit-link_text">visit</p>
                 {item.arrowIcon &&
                   React.createElement(item.arrowIcon, {
-                    className: "visit_link_icon",
+                    className: "other_restaurant_visit_link_icon",
                   })}
               </Link>
             </div>
