7dce3751645c280f49d8d0677b35923f67acc22b
Oyinloye Joseph <oyinjoe23@gmail.com>
2024-09-10 15:26:04 +0100

    feat: profile page > delivery-setup > change edit icon

--- containers/profile/delivery-setup/DeliverySetup.tsx
+++ containers/profile/delivery-setup/DeliverySetup.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AiOutlineDelete } from "react-icons/ai";
-import { CiEdit } from "react-icons/ci";
+import { BiEditAlt } from "react-icons/bi";
 import { ToggleIcon } from "./ToggleIcon";
 import "./delivery-setup.css";
 
@@ -12,56 +12,56 @@ export const DeliverySetup = () => {
           Add Now
         </button>
       </div>
-      
+
       <div className="delivery_address_container">
-      <div className="delivery_address_content content_1">
-        <div className="question_det_connt">
-          <p className="address_question">Are you still living here?</p>
-          <div className="det_container">
-            <AiOutlineDelete className="det_container_icon det_delete" />
-            <CiEdit className="det_container_icon det_edit" />
-            <ToggleIcon />
+        <div className="delivery_address_content content_1">
+          <div className="question_det_connt">
+            <p className="address_question">Are you still living here?</p>
+            <div className="det_container">
+              <AiOutlineDelete className="det_container_icon det_delete" />
+              <BiEditAlt className="det_container_icon det_edit" />
+              <ToggleIcon />
+            </div>
+          </div>
+
+          <div className="delivery_address_area">
+            <label htmlFor="address" className="delivery_address_label">
+              Address
+            </label>
+            <textarea
+              name="Address"
+              id="address"
+              rows={10}
+              cols={10}
+              className="delivery_address_textarea"
+              placeholder="No 24 Eberechi street, umuahia, Eberechi street, umuahia, Abia state."
+            ></textarea>
           </div>
         </div>
+        <div className="delivery_address_content content_2">
+          <div className="question_det_connt">
+            <p className="address_question">Are you still living here?</p>
+            <div className="det_container">
+              <AiOutlineDelete className="det_container_icon det_delete" />
+              <BiEditAlt className="det_container_icon det_edit" />
+              <ToggleIcon />
+            </div>
+          </div>
 
-        <div className="delivery_address_area">
-          <label htmlFor="address" className="delivery_address_label">
-            Address
-          </label>
-          <textarea
-            name="Address"
-            id="address"
-            rows={10}
-            cols={10}
-            className="delivery_address_textarea"
-            placeholder="No 24 Eberechi street, umuahia, Eberechi street, umuahia, Abia state."
-          ></textarea>
-        </div>
-      </div>
-      <div className="delivery_address_content content_2">
-        <div className="question_det_connt">
-          <p className="address_question">Are you still living here?</p>
-          <div className="det_container">
-            <AiOutlineDelete className="det_container_icon det_delete" />
-            <CiEdit className="det_container_icon det_edit" />
-            <ToggleIcon />
+          <div className="delivery_address_area">
+            <label htmlFor="address" className="delivery_address_label">
+              Address
+            </label>
+            <textarea
+              name="Address"
+              id="address"
+              rows={10}
+              cols={10}
+              className="delivery_address_textarea"
+              placeholder="No 24 Eberechi street, umuahia, Eberechi street, umuahia, Abia state."
+            ></textarea>
           </div>
         </div>
-
-        <div className="delivery_address_area">
-          <label htmlFor="address" className="delivery_address_label">
-            Address
-          </label>
-          <textarea
-            name="Address"
-            id="address"
-            rows={10}
-            cols={10}
-            className="delivery_address_textarea"
-            placeholder="No 24 Eberechi street, umuahia, Eberechi street, umuahia, Abia state."
-          ></textarea>
-        </div>
-      </div>
       </div>
     </div>
   );
