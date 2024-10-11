2f327adcde6b216068bef905a443c512b3e2d3e8
Oyinloye Joseph <ojtech2003@gmail.com>
2024-10-11 14:24:59 +0100

    feat: compeleted Moving Reviews page

--- containers/MovingDetails/components/MovingBRP/containers/MovingPost/MovingPost.tsx
+++ containers/MovingDetails/components/MovingBRP/containers/MovingPost/MovingPost.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './MovingPost.module.css'
 export const MovingPost = () => {
   return (
-    <div>MovingPost</div>
+    <>
+    <VendorPost />
+    </>
   )
 }
--- containers/MovingDetails/components/MovingBRP/containers/MovingReviews/MovingReviews.tsx
+++ containers/MovingDetails/components/MovingBRP/containers/MovingReviews/MovingReviews.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import styles from './MovingReviews.module.css'
+import { CustomersRating } from  "./components/CustomersRating"
+import { CustomersReview } from  "./components/CustomersReview"
 export const MovingReviews = () => {
   return (
-    <div>MovingReviews</div>
+    <div className={styles.MovingReviewContainer}>
+      <div className={styles.MovingReviewCards}>
+        <CustomersRating />
+        <CustomersReview />
+    </div>
+    </div>
   )
 }
